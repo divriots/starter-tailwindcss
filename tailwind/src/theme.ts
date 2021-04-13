aa34bfb66ce25603e461f031ee7969376a51275b
mveres <veres85@yahoo.com>
2021-04-13 17:07:20 +0300

    remove unneeded setup call

--- tailwind/src/theme.ts
+++ tailwind/src/theme.ts
@@ -1,6 +1,4 @@
 import resolveConfig from 'tailwindcss/resolveConfig';
-import { setup } from './setup';
 import { partialTheme } from './partial-theme';
 
-setup();
 export const { theme } = resolveConfig({ theme: partialTheme });
